8eb5d5559fbe86102e1012c856e59d1a650a9b81
pedrokekww <pedroveb119@gmail.com>
2025-11-26 21:24:37 -0300

    atividade sete

--- Atividades/atividade-7/README.md
+++ Atividades/atividade-7/README.md
@@ -1,4 +1,1 @@
-# avaliação 7
-Executar o Codelab Anexo. Mostrar a sua implementação no desktop.
-Evidenciar o código no Github.
-Postar o link do Github no Google Classroom.
+# atividade 7
---
+++ Atividades/atividade-7/github_client/lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'Ov23liVIrgZS7fkSyPZW';
+const githubClientSecret = 'c6a9aca85b03615c46d96846ab614f2092480770';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
